f7c26082ecb79e3f615c47601f4acf829b567aa1
Santiago Solana <santiago.sjs@gmail.com>
2020-09-19 10:25:14 -0400

    added new fields to user table

--- models/user.js
+++ models/user.js
@@ -11,12 +11,34 @@ module.exports = function(sequelize, DataTypes) {
       validate: {
         isEmail: true
       }
-    }, 
+    },
     // The password cannot be null
     password: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 160],
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 160],
+      }
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 160]
+      }
     }
+
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
   User.prototype.validPassword = function(password) {
