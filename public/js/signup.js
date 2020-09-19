9e7ad1681fe562f71fdb64af3a152bb491516553
Erika <erika@beasleys.com>
2020-09-19 10:53:55 -0400

    added additional signup information in js

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,6 +1,9 @@
 $(document).ready(() => {
   // Getting references to our form and input
   const signUpForm = $("form.signup");
+  const fnameInput = $("input#fname-input");
+  const lnameInput = $("input#lname-input");
+  const jobSelect = $("select#job-select");
   const emailInput = $("input#email-input");
   const passwordInput = $("input#password-input");
 
@@ -8,23 +11,32 @@ $(document).ready(() => {
   signUpForm.on("submit", event => {
     event.preventDefault();
     const userData = {
+      fname: fnameInput.val().trim(),
+      lname: lnameInput.val().trim(),
+      job: jobSelect.val().trim(), //probably don't need this
       email: emailInput.val().trim(),
       password: passwordInput.val().trim()
     };
 
-    if (!userData.email || !userData.password) {
+    if (!userData.fname || !userData.lname || !userData.job || !userData.email || !userData.password) {
       return;
     }
     // If we have an email and password, run the signUpUser function
-    signUpUser(userData.email, userData.password);
+    signUpUser(userData.fname, userData.lname, userData.job, userData.email, userData.password);
+    fnameInput.val("");
+    lnameInput.val("");
+    jobSelect.val("");
     emailInput.val("");
     passwordInput.val("");
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(email, password) {
+  function signUpUser(fname, lname, job, email, password) {
     $.post("/api/signup", {
+      fname: fname,
+      lname: lname,
+      job: job,
       email: email,
       password: password
     })
