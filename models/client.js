3883b1534e74475433746543c42e7caed2d4b816
Santiago Solana <santiago.sjs@gmail.com>
2020-09-19 11:03:04 -0400

    Created Client table

---
+++ models/client.js
@@ -0,0 +1,52 @@
+module.exports = function(sequelize, DataTypes) {
+  const Client = sequelize.define("Client", {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 160]
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 160]
+      }
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 160]
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    phoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [10]
+      }
+    },
+    company: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 160]
+      }
+    }
+  });
+
+  // Add a belongsTo association to Authors here
+  // Example: https://github.com/sequelize/express-example/blob/master/models/task.js
+  return Post;
+};
