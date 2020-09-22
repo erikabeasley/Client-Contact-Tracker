61ca5429d34e0179ed722b66544af763bf6cfd39
Santiago Solana <santiago.sjs@gmail.com>
2020-09-22 17:06:24 -0400

    cleaned up table in dashboard

--- public/js/members.js
+++ public/js/members.js
@@ -1,30 +1,45 @@
 $(document).ready(() => {
-  // This file just does a GET request to figure out which user is logged in
-  // and updates the HTML on the page
+ 
   const addClient = $("#addClient");
-  const allClientsBox = $("#allClientsInfo");
+  const clientTable = $("#clientTable");
 
+  // API call for accesing user_data
   $.get("/api/user_data").then(data => {
     $(".member-name").text(data.firstName + " " + data.lastName);
   });
+
+  // API call for accesing all clients data
   $.get("/api/allClients").then(data =>{
     console.log(data);
     displayAllClients(data);
   });
+
+  // add new client click event
   addClient.on("click", event => {
     event.preventDefault();
     window.location = "/newClient";
     console.log("clicked");
   });
 
+  // Funtion for displaying our clients
   const displayAllClients = clients => {
     console.log(clients);
     clients.forEach(client => {
-      allClientsBox.append(`
-      <button class="contact-btn">
-          <h3 class="text-center"><a>${client.firstName} ${client.lastName}</a> | ${client.title} | ${client.company} | ${client.email} </h3>
-      </button>
+      clientTable.append(`
+      <tr class="clientInfo" data-id="${client.id}">
+            <td>${client.firstName} ${client.lastName}</td>
+            <td>${client.title}</td>
+            <td>${client.title}</td>
+            <td>${client.email}</td>
+      </tr>     
       `);
     });
   };
+  $(window).on("load", () => {
+
+    $(".clientInfo").on("click", event => {
+      event.preventDefault();
+      console.log(this);
+    });
+  });
 });
